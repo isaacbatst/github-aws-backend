c0f454485255548af7203ef3845d71654bc62403
Isaac Batista <isaacbatst@gmail.com>
2020-01-09 21:43:36 -0300

    refactor: changes directories

---
+++ resources/repos/get.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const fetch = require("node-fetch");
+
+const { generateErrorResponse } = require("../../classes/ErrorResponseGenerator");
+const { validateRequest } = require("../../classes/RequestValidator");
+
+const RequestError = require("../../classes/RequestError");
+
+module.exports.handle = async ({ pathParameters }) => {
+  try {
+    if (!isRequestValid(pathParameters)) {
+      throw new RequestError({ statusCode: 400, message: "Invalid params" });
+    }
+
+    const repos = await getRepos(pathParameters.username);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,GET"
+      },
+      body: JSON.stringify({ repos })
+    };
+  } catch (error) {
+    return generateErrorResponse(error);
+  }
+};
+
+const isRequestValid = pathParameters => {
+  return validateRequest({
+    requiredParams: ["username"],
+    receivedParams: pathParameters
+  });
+};
+
+const getRepos = async username => {
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`
+  );
+
+  if (response.status !== 200) {
+    throw new RequestError({
+      statusCode: response.status,
+      message: `Github says: ${response.statusText}` 
+    });
+  }
+
+  return await response.json();
+};
